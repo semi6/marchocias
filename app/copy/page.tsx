37967795b14371215357817a96df980ba8269bc4
taizo <kodera.t1@gmail.com>
2024-04-29 17:26:46 +0900

    fix

---
+++ app/copy/page.tsx
@@ -0,0 +1,71 @@
+"use client";
+
+import { QRCodeSVG } from 'qrcode.react';
+import { useEffect, useState } from 'react';
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+} from "@/components/ui/card"
+
+export default function Copy() {
+  type ResultObject = { [key: string]: { [key: string]: { [key: string]: number } } };
+  const [result, setResult] = useState<ResultObject>({});
+  const [resultArr, setresultArr] = useState<Array<string>>([]);
+
+  const queryStringList: any = {
+    w90: 'a',
+    w115: 'b',
+    w125: 'c',
+    w170: 'd',
+    g8: 'e',
+    g7: 'f',
+    g6: 'g',
+    g5: 'h',
+    g4: 'i',
+    g3: 'j',
+    g2: 'k',
+    g1: 'l',
+    g0: 'm',
+  }
+
+  useEffect(() => {
+    const resultData = localStorage.getItem('result-data');
+    if (resultData) {
+      const storedData = JSON.parse(resultData)
+      setResult(storedData);
+    }
+  }, []);
+
+  Object.keys(result).forEach(w => {
+    resultArr.push(queryStringList[w])
+    Object.keys(result[w]).forEach(g => {
+      resultArr.push(queryStringList[g])
+      Object.keys(result[w][g]).forEach(r => {
+        if (result[w][g][r] === 1) {
+          resultArr.push(r)
+        }
+      })
+    })
+  });
+
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-2">
+      <Card className="w-[400px]">
+        <CardHeader>
+          <CardDescription className='text-center'>QRコードで別の端末にコピーできます</CardDescription>
+        </CardHeader>
+        <CardContent className='flex justify-center'>
+          {resultArr.length > 0 && (
+            <QRCodeSVG value={`https://semi6.github.io/marchocias/import?r=${resultArr.join('')}`} size={150} />
+          )}
+        </CardContent>
+        <CardFooter className='flex justify-center'>
+          <CardDescription><a href="/">もどる</a></CardDescription>
+        </CardFooter>
+      </Card>
+    </main>
+  );
+}
---
+++ app/import/page.tsx
@@ -0,0 +1,72 @@
+"use client";
+
+import { useEffect } from 'react';
+import { permanentRedirect, useSearchParams } from "next/navigation";
+import { Card, CardDescription, CardFooter, CardHeader } from '@/components/ui/card';
+
+export default function Inport() {
+  type ResultObject = { [key: string]: { [key: string]: { [key: string]: number } } };
+  const searchParams = useSearchParams();
+
+  useEffect(() => {
+    const queryWallList: any = {
+      a: 'w90',
+      b: 'w115',
+      c: 'w125',
+      d: 'w170',
+    }
+
+    const queryGradeList: any = {
+      e: 'g8',
+      f: 'g7',
+      g: 'g6',
+      h: 'g5',
+      i: 'g4',
+      j: 'g3',
+      k: 'g2',
+      l: 'g1',
+      m: 'g0',
+    }
+
+    const resultData = localStorage.getItem('result-data');
+    let parsedResultData: ResultObject = {}
+
+    if (!resultData || !confirm('保存されたデータがあります。上書きしますか？')) {
+      const resultStrings = searchParams.get('r')
+      if (resultStrings) {
+        let w
+        let g
+        for (let i = 0; i < resultStrings.length; i++) {
+          if (queryWallList[resultStrings[i]]) {
+            w = queryWallList[resultStrings[i]]
+          } else if (queryGradeList[resultStrings[i]]) {
+            g = queryGradeList[resultStrings[i]]
+          } else {
+            parsedResultData[w] ||= {}
+            parsedResultData[w][g] ||= {}
+            parsedResultData[w][g][resultStrings[i]] = 1
+          }
+        }
+      }
+    } else {
+      parsedResultData = JSON.parse(resultData)
+    }
+
+    localStorage.setItem('result-data', JSON.stringify(parsedResultData));
+
+    permanentRedirect('/')
+  }, [searchParams]);
+
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-2">
+      <Card className="w-[400px]">
+        <CardHeader>
+          <CardDescription className='text-center'>データを保存しています...</CardDescription>
+        </CardHeader>
+        <CardFooter className='flex justify-center'>
+          <CardDescription><a href="/">もどる</a></CardDescription>
+        </CardFooter>
+      </Card>
+    </main>
+  );
+}
