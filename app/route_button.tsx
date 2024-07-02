310aeebd521e6582481b29b6cd3f7231319a5ba1
taizo <kodera.t1@gmail.com>
2024-07-02 15:19:00 +0900

    スクロール時にタップしないようにした

--- app/route_button.tsx
+++ app/route_button.tsx
@@ -13,6 +13,8 @@ type ResultObject = { [key: string]: { [key: string]: { [key: string]: number } 
 
 const RouteList: React.FC<RouteListProps> = ({ wall, grade, route, defaultValue }) => {
   const [value, setValue] = useState<boolean>(defaultValue)
+  const [startY, setStartY] = useState<number>(0)
+  const [isScroll, setIsScroll] = useState<boolean>(false)
 
   const gradeSettings: any = {
     g8: { achieveColor: 'bg-purple-100', color: 'bg-purple-600' },
@@ -27,24 +29,40 @@ const RouteList: React.FC<RouteListProps> = ({ wall, grade, route, defaultValue 
   }
 
   useEffect(() => {
-    setValue(defaultValue);
-  }, [defaultValue]);
+    setValue(defaultValue)
+  }, [defaultValue])
+
+  const handleTouchStart = (e: React.TouchEvent<HTMLButtonElement>) => {
+    setStartY(e.touches[0].pageY)
+    setIsScroll(false)
+  };
+
+  const handleTouchMove = (e: React.TouchEvent<HTMLButtonElement>) => {
+    const deltaY = Math.abs(startY - e.touches[0].pageY)
+    if (deltaY > 10) {
+      setIsScroll(true)
+    }
+  }
 
   const toggleCompleted = (tmpValue: boolean) => {
-    const resultDataJson = localStorage.getItem('result-data')
-    const resultData: ResultObject = !!resultDataJson ? JSON.parse(resultDataJson) : {}
+    if (!isScroll) {
+      const resultDataJson = localStorage.getItem('result-data')
+      const resultData: ResultObject = !!resultDataJson ? JSON.parse(resultDataJson) : {}
 
-    resultData[wall] = resultData[wall] ?? {}
-    resultData[wall]![grade] = resultData[wall]![grade] ?? {}
-    resultData[wall]![grade]![route] = tmpValue ? 1 : 0
+      resultData[wall] = resultData[wall] ?? {}
+      resultData[wall]![grade] = resultData[wall]![grade] ?? {}
+      resultData[wall]![grade]![route] = tmpValue ? 1 : 0
 
-    setValue(tmpValue)
-    localStorage.setItem('result-data', JSON.stringify(resultData))
-  };
+      setValue(tmpValue)
+      localStorage.setItem('result-data', JSON.stringify(resultData))
+    }
+  }
 
   return (
     <Button
       onClick={() => !isMobile && toggleCompleted(!value)}
+      onTouchStart={handleTouchStart}
+      onTouchMove={handleTouchMove}
       onTouchEnd={() => toggleCompleted(!value)}
       className={`ml-1 w-11 h-11 ${value ? gradeSettings[grade].achieveColor : gradeSettings[grade].color} hover:${value ? gradeSettings[grade].achieveColor : gradeSettings[grade].color}`}
     >
